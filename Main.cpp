2be4cd7e6f9a2e9f8bd1a2d20ed26aee5047eaa7
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-01-30 06:55:06 +0800

    Prototype

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,20 @@
+#include <cstring>
 #include <iostream>
 
+struct CHARACTER {
+  char name[10];
+  int money;
+}
+
 int main(void) {
+  struct CHARACTER character[8];
+
+  character[0].name = "Termy";
+
+  int8_t IsGameRunning = 1;
+
+  while (IsGameRunning == 1) {
+
+  }
   return 0;
 }
