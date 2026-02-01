b8e4538f14151a266bca78bdf83d491bff39d836
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-02-02 06:46:38 +0800

    Added products

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@
 
 struct CHARACTER {
   char name[10];
-  int money;
+  uint16_t money;
 };
 
 struct CHARACTER character[8];
@@ -26,8 +26,24 @@ character[5].money = 100;
 character[6].money = 100;
 character[7].money = 100;
 
+struct ITEM {
+  char name[10];
+  uint16_t price;
+};
+
+struct ITEM item[8];
+
+item[0].name = "Circle";
+item[1].name = "Triangle";
+item[2].name = "Square";
+item[3].name = "Rectangle";
+item[4].name = "Arrow";
+item[5].name = "Heart";
+item[6].name = "Crescent";
+item[7].name = "Star";
+
 int main(void) {
-  int8_t IsGameRunning = 1;
+  uint8_t IsGameRunning = 1;
 
   while (IsGameRunning == 1) {
 
