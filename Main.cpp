f5dd619f148af749710b67b6cefd9dd0777f15b5
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-02-02 06:33:34 +0800

    Changes

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,34 @@
 struct CHARACTER {
   char name[10];
   int money;
-}
+};
+
+struct CHARACTER character[8];
+
+character[0].name = "Termy";
+character[1].name = "";
+character[2].name = "";
+character[3].name = "";
+character[4].name = "";
+character[5].name = "";
+character[6].name = "";
+character[7].name = "";
+
+character[0].money = 20;
+character[1].money = 100;
+character[2].money = 100;
+character[3].money = 100;
+character[4].money = 100;
+character[5].money = 100;
+character[6].money = 100;
+character[7].money = 100;
 
 int main(void) {
-  struct CHARACTER character[8];
-
-  character[0].name = "Termy";
-
   int8_t IsGameRunning = 1;
 
   while (IsGameRunning == 1) {
 
   }
+
   return 0;
 }
